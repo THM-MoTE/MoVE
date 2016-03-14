30715f7a91f78c8c1b52576b79564407af64e039
N. Justus <nicola.justus@mni.thm.de>
2016-03-14 15:00:44 +0100

    Added styles & font awesome to MoveApp Added controls for drawing lines, circles, rectangels

--- src/main/scala/de/thm/move/MoveApp.scala
+++ src/main/scala/de/thm/move/MoveApp.scala
@@ -1,18 +1,27 @@
 package de.thm.move
 
+import java.util.ResourceBundle
 import javafx.application.Application
+import javafx.scene.text.Font
 import javafx.stage.Stage
 import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.fxml.FXMLLoader
 import javafx.scene.control.Label
+import javax.annotation.Resource
 
 class MoveApp extends Application {
-  def start(stage: Stage): Unit = {    
+  def start(stage: Stage): Unit = {
+    val styleSheetUrl = MoveApp.getClass.getResource("/style.css").toExternalForm
+    //val fontAwesome = Font.loadFont(MoveApp.getClass.getResource("/fontawesome-webfont.ttf").toExternalForm, 10)
+
     val fxmlLoader = new FXMLLoader(MoveApp.getClass.getResource("/de/thm/move/views/move.fxml"))
+    val bundle = ResourceBundle.getBundle("fontawesome")
+    fxmlLoader.setResources(bundle)
     val mainViewRoot: Parent = fxmlLoader.load()   
-    val scene = new Scene(mainViewRoot);
-    
+    val scene = new Scene(mainViewRoot)
+    scene.getStylesheets.add(styleSheetUrl)
+
     stage.setTitle("Move")
     stage.setScene(scene)
     stage.setWidth(600)
--- src/main/scala/de/thm/move/controllers/MoveCtrl.scala
+++ src/main/scala/de/thm/move/controllers/MoveCtrl.scala
@@ -1,22 +1,79 @@
 package de.thm.move.controllers
 
+import java.net.URL
+import java.util.ResourceBundle
 import javafx.event.ActionEvent
-import javafx.fxml.FXML
-import javafx.scene.canvas.Canvas
+import javafx.fxml.{Initializable, FXML}
+import javafx.scene.canvas.{GraphicsContext, Canvas}
+import javafx.scene.control.{ColorPicker, ToggleGroup, Button}
+import javafx.scene.input.MouseEvent
+import javafx.scene.paint.Color
 
-class MoveCtrl {
+import implicits.FxHandlerImplicits._
+
+class MoveCtrl extends Initializable {
   
   @FXML
   private var mainCanvas: Canvas = _
-  
+  @FXML
+  private var btnGroup: ToggleGroup = _
+  @FXML
+  private var  colorPicker: ColorPicker = _
+
+  override def initialize(location: URL, resources: ResourceBundle): Unit = {
+    colorPicker.setValue(Color.BLACK)
+
+    val drawHandler = { mouseEvent:MouseEvent =>
+
+    }
+
+    mainCanvas.setOnMousePressed(drawHandler)
+    mainCanvas.setOnMousePressed(drawHandler)
+
+  }
+
+  @FXML
+  def onPointerClicked(e:ActionEvent): Unit = {
+    //println( btnGroup.getSelectedToggle )
+    println("pointer clicked")
+  }
+
+
   @FXML
   def onCircleClicked(e:ActionEvent): Unit = {
-    mainCanvas.getGraphicsContext2D.fillOval(10, 60, 30, 30);
+    drawColored { canvas =>
+      canvas.fillOval(10, 60, 30, 30)
+    }
+
+    println( btnGroup.getSelectedToggle )
     println("Circle clicked")
   }
   
   @FXML
   def onRectangleClicked(e:ActionEvent): Unit = {
+    drawColored { canvas =>
+      canvas.fillRect(0, 0, 20, 20)
+    }
+    println( btnGroup.getSelectedToggle )
     println("rect clicked")
+  }
+
+  @FXML
+  def onLineClicked(e:ActionEvent): Unit = {
+    drawColored { canvas =>
+      canvas.strokeLine(5,5, 10,100)
+    }
+
+    println("line clicked")
+  }
+
+  private def getForegroundColor: Color = colorPicker.getValue
+
+  private def drawColored[A](fn: GraphicsContext => A): A = {
+    val context = mainCanvas.getGraphicsContext2D
+    val color = getForegroundColor
+    context.setFill(color)
+    context.setStroke(color)
+    fn(context)
   }
 }
