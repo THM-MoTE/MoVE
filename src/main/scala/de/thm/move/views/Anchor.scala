aa0a8e13942428231ef74f796122ec448821eb7f
N. Justus <nicola.justus@mni.thm.de>
2016-03-15 15:49:22 +0100

    Added custom anchor

---
+++ src/main/scala/de/thm/move/views/Anchor.scala
@@ -0,0 +1,17 @@
+package de.thm.move.views
+
+import javafx.scene.paint.Color
+import javafx.scene.shape.{Ellipse, Circle}
+
+class Anchor(x:Double, y:Double, fillColor:Color) extends Ellipse {
+  this.setCenterX(x)
+  this.setCenterY(y)
+  this.setFill(fillColor)
+  this.setRadiusX(Anchor.anchorWeight)
+  this.setRadiusY(Anchor.anchorHeight)
+}
+
+object Anchor {
+  val anchorWeight = 5.0
+  val anchorHeight = 5.0
+}
