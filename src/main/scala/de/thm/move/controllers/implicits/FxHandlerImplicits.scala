348cc05b4175dfc2b378afe6758752c04fb5a018
N. Justus <nicola.justus@mni.thm.de>
2016-03-16 14:30:44 +0100

    Added implicit conversion for changelisteners

--- src/main/scala/de/thm/move/controllers/implicits/FxHandlerImplicits.scala
+++ src/main/scala/de/thm/move/controllers/implicits/FxHandlerImplicits.scala
@@ -1,5 +1,6 @@
 package de.thm.move.controllers.implicits
 
+import javafx.beans.value.{ChangeListener, ObservableValue}
 import javafx.event.{ActionEvent, EventHandler}
 import javafx.scene.input.{InputEvent, MouseEvent}
 
@@ -10,5 +11,9 @@ object FxHandlerImplicits {
 
   implicit def actionEventHandler[T >: ActionEvent](fn: T => Unit): EventHandler[ActionEvent] = new EventHandler[ActionEvent]() {
     override def handle(event: ActionEvent): Unit = fn(event)
+  }
+
+  implicit def changeListener[A](fn: (ObservableValue[_<:A], A, A) => Unit) = new ChangeListener[A] {
+    override def changed(observable: ObservableValue[_ <: A], oldValue: A, newValue: A): Unit = fn(observable, oldValue, newValue)
   }
 }
