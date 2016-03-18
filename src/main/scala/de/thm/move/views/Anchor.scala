c29fb0e9ea377fd7d6f9a7543447bc0b6f158273
N. Justus <nicola.justus@mni.thm.de>
2016-03-18 13:50:49 +0100

    Moved coloring for anchors into css-class anchor

--- src/main/scala/de/thm/move/views/Anchor.scala
+++ src/main/scala/de/thm/move/views/Anchor.scala
@@ -4,12 +4,12 @@ import javafx.scene.paint.Color
 import javafx.scene.shape.{Ellipse, Circle}
 import de.thm.move.models.CommonTypes.Point
 
-class Anchor(x:Double, y:Double, fillColor:Color = Anchor.anchorFill) extends Ellipse {
+class Anchor(x:Double, y:Double) extends Ellipse {
   def this(p:Point) = this(p._1, p._2)
 
+  this.getStyleClass.addAll("anchor")
   this.setCenterX(x)
   this.setCenterY(y)
-  this.setFill(fillColor)
   this.setRadiusX(Anchor.anchorWeight)
   this.setRadiusY(Anchor.anchorHeight)
 }
@@ -17,5 +17,4 @@ class Anchor(x:Double, y:Double, fillColor:Color = Anchor.anchorFill) extends El
 object Anchor {
   val anchorWeight = 5.0
   val anchorHeight = 5.0
-  val anchorFill = Color.RED
 }
