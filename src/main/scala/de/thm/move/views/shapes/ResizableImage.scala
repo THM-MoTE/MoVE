88c4abc4fdfb23850b541f403d78d3f99baafa2f
N. Justus <nicola.justus@mni.thm.de>
2016-03-18 14:37:24 +0100

    Made image resizable.

--- src/main/scala/de/thm/move/views/shapes/ResizableImage.scala
+++ src/main/scala/de/thm/move/views/shapes/ResizableImage.scala
@@ -12,6 +12,40 @@ class ResizableImage(img:Image) extends ImageView(img) with ResizableShape with 
   setPreserveRatio(true)
   setFitWidth(200)
 
+  topLeftAnchor.setOnMouseDragged({me:MouseEvent =>
+    val (newX, newY) = (me.getX, me.getY)
+    val (oldX, oldY) = (this.getX, this.getY)
+    val deltaY = if(newY < oldY) getFitHeight + (oldY-newY)else getFitHeight - (newY-oldY)
+    val deltaX = if(newX > oldX) getFitWidth + (oldX-newX) else getFitWidth - (newX-oldX)
+    this.setY(newY)
+    this.setFitHeight(deltaY)
+    this.setX(newX)
+    this.setFitWidth(deltaX)
+  })
+
+  topRightAnchor.setOnMouseDragged({ me: MouseEvent =>
+    val (newX, newY) = (me.getX, me.getY)
+    val (oldX, oldY) = (this.getX+this.getFitWidth, this.getY)
+    val deltaY = if(newY < oldY) getFitHeight + (oldY-newY) else getFitHeight - (newY-oldY)
+    val deltaX = if(newX > oldX) getFitWidth + (newX-oldX) else getFitWidth - (oldX-newX)
+
+    this.setY(newY)
+    this.setFitHeight(deltaY)
+    this.setFitWidth(deltaX)
+  })
+
+  bottomLeftAnchor.setOnMouseDragged({ me: MouseEvent =>
+    val (newX, newY) = (me.getX, me.getY)
+    val (oldX, oldY) = (this.getX, this.getY+getFitHeight)
+
+    val deltaY = (newY-oldY) + getFitHeight
+    val deltaX = (oldX-newX) + getFitWidth
+
+    this.setX(newX)
+    this.setFitHeight(deltaY)
+    this.setFitWidth(deltaX)
+  })
+
   bottomRightAnchor.setOnMouseDragged({ me: MouseEvent =>
     val (newX, newY) = (me.getX, me.getY)
     val (oldX, oldY) = (this.getX+this.getFitWidth, this.getY+this.getFitHeight)
