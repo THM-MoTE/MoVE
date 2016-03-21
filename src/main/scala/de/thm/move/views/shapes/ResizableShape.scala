620b2ca0ebb178cb7b3112b7e69a3e256f049ff1
N. Justus <nicola.justus@mni.thm.de>
2016-03-21 10:05:19 +0100

    Abstracted s-/getX/Y, s-/getWidth, s-/getHeight into ResizableShape. All implementing classes override for their specific behaviour.

--- src/main/scala/de/thm/move/views/shapes/ResizableShape.scala
+++ src/main/scala/de/thm/move/views/shapes/ResizableShape.scala
@@ -4,4 +4,14 @@ import de.thm.move.views.Anchor
 
 trait ResizableShape {
   def getAnchors: List[Anchor]
+
+  def getX: Double
+  def getY: Double
+  def getWidth: Double
+  def getHeight: Double
+
+  def setX(x:Double): Unit
+  def setY(y:Double): Unit
+  def setWidth(w:Double): Unit
+  def setHeight(h:Double): Unit
 }
