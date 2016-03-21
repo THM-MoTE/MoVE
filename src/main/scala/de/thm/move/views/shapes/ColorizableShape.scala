56c0be25bc358067830f3e765a0925b984444910
N. Justus <nicola.justus@mni.thm.de>
2016-03-21 15:33:11 +0100

    Added undo-/redo for colorizing shapes.

--- src/main/scala/de/thm/move/views/shapes/ColorizableShape.scala
+++ src/main/scala/de/thm/move/views/shapes/ColorizableShape.scala
@@ -3,15 +3,31 @@ package de.thm.move.views.shapes
 import javafx.scene.paint.Color
 import javafx.scene.shape.Shape
 
+import de.thm.move.Global
+import de.thm.move.history.History
+import de.thm.move.history.History.Command
+
 trait ColorizableShape {
   self: Shape =>
 
   def colorizeShape(fillColor:Color, strokeColor:Color): Unit = {
-    self.setFill(fillColor)
-    self.setStroke(strokeColor)
+    self.setFillColor(fillColor)
+    self.setStrokeColor(strokeColor)
   }
 
-  def setStrokeWidth(width:Int): Unit = self.strokeWidthProperty().set(width)
-  def setFillColor(c:Color): Unit = self.setFill(c)
-  def setStrokeColor(c:Color): Unit = self.setStroke(c)
+  def setStrokeWidth(width:Int): Unit = {
+    val oldWidth = self.strokeWidthProperty().get()
+    val cmd = History.newCommand(self.strokeWidthProperty().set(width), self.strokeWidthProperty().set(oldWidth))
+    Global.history.execute(cmd)
+  }
+  def setFillColor(c:Color): Unit = {
+    val oldColor = self.getFill
+    val cmd =  History.newCommand(self.setFill(c), self.setFill(oldColor))
+    Global.history.execute(cmd)
+  }
+  def setStrokeColor(c:Color): Unit = {
+    val oldColor = self.getFill
+    val cmd =  History.newCommand(self.setStroke(c), self.setStroke(oldColor))
+    Global.history.execute(cmd)
+  }
 }
