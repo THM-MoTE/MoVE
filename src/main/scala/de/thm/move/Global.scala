16bb3f5d08c554b4210fff606f5cc7d6dff6952c
N. Justus <nicola.justus@mni.thm.de>
2016-03-22 12:14:53 +0100

    Created Global config value. Changed filepath from String to URL.

--- src/main/scala/de/thm/move/Global.scala
+++ src/main/scala/de/thm/move/Global.scala
@@ -1,7 +1,11 @@
 package de.thm.move
 
 import de.thm.move.history.History
+import de.thm.move.config.Config
+import de.thm.move.config.ConfigLoader
 
 object Global {
+  
+  lazy val config: Config = new ConfigLoader(getClass.getResource("/move.conf"))
   lazy val history = new History()
 }
--- src/main/scala/de/thm/move/config/ConfigLoader.scala
+++ src/main/scala/de/thm/move/config/ConfigLoader.scala
@@ -3,9 +3,9 @@ package de.thm.move.config
 import scala.io.Source
 import java.net.URL
 
-class ConfigLoader(path:String) extends Config {
+class ConfigLoader(path:URL) extends Config {
   private val map =
-    Source.fromURL(new URL(path), "UTF_8").getLines().flatMap { line =>
+    Source.fromURL(path, "UTF_8").getLines().flatMap { line =>
       val splitted = line.split("=")
       if(splitted.length != 2) List()
       else List( (splitted.head, splitted.tail.head) )
