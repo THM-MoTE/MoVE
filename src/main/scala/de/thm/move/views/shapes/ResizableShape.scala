6f0b9a71f97a2b30d3c406fee0d2b4919fb457b2
N. Justus <nicola.justus@mni.thm.de>
2016-03-22 15:43:25 +0100

    Added property that indicates if the shape should be resized with preserveRatio.

--- src/main/scala/de/thm/move/views/shapes/ResizableShape.scala
+++ src/main/scala/de/thm/move/views/shapes/ResizableShape.scala
@@ -1,12 +1,35 @@
 package de.thm.move.views.shapes
 
 import javafx.scene.Node
-
+import javafx.beans.property.SimpleBooleanProperty
+import javafx.scene.input.KeyEvent
+import javafx.scene.input.KeyCode
 import de.thm.move.views.Anchor
+import de.thm.move.Global
+import de.thm.move.controllers.implicits.FxHandlerImplicits._
 
 trait ResizableShape extends Node {
 
+  val resizeProportionalProperty = new SimpleBooleanProperty(false)
   val selectionRectangle = new SelectionRectangle(this)
+
+  private def ifKeyMatches(code:KeyCode, fileKey:String)(fn : => Unit): Unit = {
+    Global.shortcuts.getKeyCode(fileKey).
+    filter(_==code).foreach { _ => fn }
+  }
+
+  setOnKeyPressed { ke:KeyEvent =>
+      ifKeyMatches(ke.getCode(), "resize-proportional") {
+          resizeProportionalProperty.set(true)
+      }
+  }
+
+  setOnKeyReleased { ke:KeyEvent =>
+    ifKeyMatches(ke.getCode(), "resize-proportional") {
+      resizeProportionalProperty.set(false)
+    }
+  }
+
 
   def getAnchors: List[Anchor]
 
