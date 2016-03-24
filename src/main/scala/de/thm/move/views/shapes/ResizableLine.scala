66e3d1e555d0d43a2b8baf0dc2b25ee678b0b58c
N. Justus <nicola.justus@mni.thm.de>
2016-03-24 09:52:44 +0100

    Reverse anchors-position control for lines
    The control is the same as for rectangles,
    circles and images!
    
    Anchors get positioned relative to lines
    startX/startY, endX/endY properties.
    They are no longer  MovableAnchors.

--- src/main/scala/de/thm/move/views/shapes/ResizableLine.scala
+++ src/main/scala/de/thm/move/views/shapes/ResizableLine.scala
@@ -5,11 +5,13 @@
 
 package de.thm.move.views.shapes
 
+import javafx.scene.input.MouseEvent
 import javafx.scene.shape.Line
 
 import de.thm.move.models.CommonTypes.Point
 import de.thm.move.util.BindingUtils
 import de.thm.move.views.{MovableAnchor, Anchor}
+import de.thm.move.controllers.implicits.FxHandlerImplicits._
 
 class ResizableLine(
          start:Point,
@@ -17,16 +19,31 @@ class ResizableLine(
          strokeSize:Int) extends Line(start._1, start._2, end._1, end._2) with ResizableShape with ColorizableShape {
   setStrokeWidth(strokeSize)
 
-  val startAnchor = new Anchor(start) with de.thm.move.views.MovableAnchor
-  val endAnchor = new Anchor(end) with de.thm.move.views.MovableAnchor
+  val startAnchor = new Anchor(start)
+  val endAnchor = new Anchor(end)
   val getAnchors: List[Anchor] = List(startAnchor, endAnchor)
+
+  startAnchor.centerXProperty().bind(startXProperty())
+  startAnchor.centerYProperty().bind(startYProperty())
+  endAnchor.centerXProperty().bind(endXProperty())
+  endAnchor.centerYProperty().bind(endYProperty())
+
+  startAnchor.setOnMouseDragged { (me:MouseEvent) =>
+    setStartX(me.getX)
+    setStartY(me.getY)
+  }
+
+  endAnchor.setOnMouseDragged { (me:MouseEvent) =>
+    setEndX(me.getX)
+    setEndY(me.getY)
+  }
 
   BindingUtils.binAnchorsLayoutToNodeLayout(this)(getAnchors:_*)
 
-  startXProperty().bind(startAnchor.centerXProperty())
-  startYProperty().bind(startAnchor.centerYProperty())
-  endXProperty().bind(endAnchor.centerXProperty())
-  endYProperty().bind(endAnchor.centerYProperty())
+//  startXProperty().bind(startAnchor.centerXProperty())
+//  startYProperty().bind(startAnchor.centerYProperty())
+//  endXProperty().bind(endAnchor.centerXProperty())
+//  endYProperty().bind(endAnchor.centerYProperty())
 
   override def getX: Double = getLayoutX
   override def setY(y: Double): Unit = setLayoutY(y)
