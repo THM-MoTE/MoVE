f5d319907db15235141f3bf0d3d01d3839b3f275
N. Justus <nicola.justus@mni.thm.de>
2016-03-25 13:44:03 +0100

    Removed unnecessary code
    - Overwriting getTopLeft isn't needed
    - Commented code isn't needed

--- src/main/scala/de/thm/move/views/shapes/ResizableImage.scala
+++ src/main/scala/de/thm/move/views/shapes/ResizableImage.scala
@@ -16,54 +16,7 @@ import de.thm.move.views.Anchor
 class ResizableImage(img:Image) extends ImageView(img) with ResizableShape with BoundedAnchors {
   setPreserveRatio(true)
   setFitWidth(200)
-/*
-  topLeftAnchor.setOnMouseDragged({me:MouseEvent =>
-    val (newX, newY) = (me.getX, me.getY)
-    val (oldX, oldY) = (this.getX, this.getY)
-    val deltaY = if(newY < oldY) getFitHeight + (oldY-newY)else getFitHeight - (newY-oldY)
-    val deltaX = if(newX > oldX) getFitWidth + (oldX-newX) else getFitWidth - (newX-oldX)
-    this.setY(newY)
-    this.setFitHeight(deltaY)
-    this.setX(newX)
-    this.setFitWidth(deltaX)
-  })
 
-  topRightAnchor.setOnMouseDragged({ me: MouseEvent =>
-    val (newX, newY) = (me.getX, me.getY)
-    val (oldX, oldY) = (this.getX+this.getFitWidth, this.getY)
-    val deltaY = if(newY < oldY) getFitHeight + (oldY-newY) else getFitHeight - (newY-oldY)
-    val deltaX = if(newX > oldX) getFitWidth + (newX-oldX) else getFitWidth - (oldX-newX)
-
-    this.setY(newY)
-    this.setFitHeight(deltaY)
-    this.setFitWidth(deltaX)
-  })
-
-  bottomLeftAnchor.setOnMouseDragged({ me: MouseEvent =>
-    val (newX, newY) = (me.getX, me.getY)
-    val (oldX, oldY) = (this.getX, this.getY+getFitHeight)
-
-    val deltaY = (newY-oldY) + getFitHeight
-    val deltaX = (oldX-newX) + getFitWidth
-
-    this.setX(newX)
-    this.setFitHeight(deltaY)
-    this.setFitWidth(deltaX)
-  })
-
-  bottomRightAnchor.setOnMouseDragged({ me: MouseEvent =>
-    val (newX, newY) = (me.getX, me.getY)
-    val (oldX, oldY) = (this.getX+this.getFitWidth, this.getY+this.getFitHeight)
-    val deltaX = (newX-oldX) + this.getFitWidth
-    val deltaY = (newY-oldY) + this.getFitWidth
-
-    setFitWidth(deltaX)
-    setFitHeight(deltaY)
-  })
-
-  */
-
-  override def getTopLeft:Point = (getX, getY)
   override def getWidth: Double = getFitWidth
   override def getHeight: Double = getFitHeight
 
