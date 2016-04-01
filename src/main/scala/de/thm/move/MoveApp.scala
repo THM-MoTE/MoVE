7d4709111b6806923f3c3699c438dd1d6116bdad
N. Justus <nicola.justus@mni.thm.de>
2016-04-01 14:39:48 +0200

    Add error-dialog if config-files are missing

--- src/main/scala/de/thm/move/MoveApp.scala
+++ src/main/scala/de/thm/move/MoveApp.scala
@@ -7,20 +7,41 @@ package de.thm.move
 
 import java.util.ResourceBundle
 import javafx.application.Application
+import javafx.scene.control.Alert.AlertType
 import javafx.scene.text.Font
 import javafx.stage.Stage
 import javafx.scene.{Cursor, Parent, Scene}
 import javafx.fxml.FXMLLoader
-import javafx.scene.control.Label
+import javafx.scene.control.{Alert, Label}
 import javax.annotation.Resource
 
 import de.thm.move.controllers.MoveCtrl
 
 class MoveApp extends Application {
+  def checkExistingConfigs(): Unit = {
+    try {
+      Global.config.getString("window.title")
+      Global.shortcuts.getKeyCode("")
+    }catch {
+      case _: NullPointerException =>
+        val errorDialog = new Alert(AlertType.ERROR)
+        errorDialog.setTitle("Loading Error")
+        errorDialog.setHeaderText("Can't load configuration files!")
+        errorDialog.setContentText(
+          "In order to run this program you need configuration files in the conf/ directory.\n" +
+          "If they don't exist this program won't load."
+        )
+
+        errorDialog.showAndWait()
+        System.exit(-1)
+    }
+  }
+
   def start(stage: Stage): Unit = {
+    checkExistingConfigs()
+
     val windowWidth = Global.config.getDouble("window.width").getOrElse(600.0)
     val windowHeight = Global.config.getDouble("window.height").getOrElse(600.0)
-
 
     val fxmlLoader = new FXMLLoader(MoveApp.getClass.getResource("/fxml/move.fxml"))
 
