1148aa7a19d03020ec8f1e343068a2a33e55da87
N. Justus <nicola.justus@mni.thm.de>
2016-04-01 15:37:31 +0200

    Make path resizable
    Resizing is implemented as in Polygon:
    Anchors are movable and the points are bounded to the anchors

--- src/main/scala/de/thm/move/views/shapes/ResizablePath.scala
+++ src/main/scala/de/thm/move/views/shapes/ResizablePath.scala
@@ -1,13 +1,28 @@
 package de.thm.move.views.shapes
 
 import javafx.scene.paint.Paint
-import javafx.scene.shape.{PathElement, Path}
+import javafx.scene.shape.{MoveTo, LineTo, PathElement, Path}
 import collection.JavaConversions._
 
-import de.thm.move.views.Anchor
+import de.thm.move.views.{MovableAnchor, Anchor}
 
-class ResizablePath(elements:Seq[PathElement]) extends Path(elements) with ResizableShape with ColorizableShape {
-  override def getAnchors: List[Anchor] = Nil
+class ResizablePath(startPoint: MoveTo, elements:List[LineTo]) extends Path(startPoint :: elements) with ResizableShape with ColorizableShape {
+
+  private val observableElements = getElements
+
+  override def getAnchors: List[Anchor] =
+    (startPoint :: elements).flatMap {
+      case move:MoveTo =>
+        val anchor = new Anchor(move.getX,move.getY) with MovableAnchor
+        move.xProperty.bind(anchor.centerXProperty)
+        move.yProperty.bind(anchor.centerYProperty)
+        List(anchor)
+      case line:LineTo =>
+        val anchor = new Anchor(line.getX,line.getY) with MovableAnchor
+        line.xProperty.bind(anchor.centerXProperty)
+        line.yProperty.bind(anchor.centerYProperty)
+        List(anchor)
+    }
 
   override def setY(y: Double): Unit = setLayoutY(y)
 
