fd1bd0e556b50070925cae2f100f0a210a34d068
N. Justus <nicola.justus@mni.thm.de>
2016-04-01 15:21:41 +0200

    Make path movable

--- src/main/scala/de/thm/move/views/shapes/ResizablePath.scala
+++ src/main/scala/de/thm/move/views/shapes/ResizablePath.scala
@@ -9,13 +9,13 @@ import de.thm.move.views.Anchor
 class ResizablePath(elements:Seq[PathElement]) extends Path(elements) with ResizableShape with ColorizableShape {
   override def getAnchors: List[Anchor] = Nil
 
-  override def setY(y: Double): Unit = ???
+  override def setY(y: Double): Unit = setLayoutY(y)
 
-  override def getY: Double = ???
+  override def getY: Double = getLayoutY
 
-  override def setX(x: Double): Unit = ???
+  override def setX(x: Double): Unit = setLayoutX(x)
 
-  override def getX: Double = ???
+  override def getX: Double = getLayoutX
 
   override def getFillColor:Paint = null
   override def setFillColor(c:Paint):Unit = Unit
