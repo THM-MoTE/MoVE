2a8439192d1b7e82486521ad0b66dda8d130aa37
N. Justus <nicola.justus@mni.thm.de>
2016-04-02 20:26:46 +0200

    Simplify function calls

--- src/main/scala/de/thm/move/controllers/factorys/ShapeFactory.scala
+++ src/main/scala/de/thm/move/controllers/factorys/ShapeFactory.scala
@@ -6,9 +6,7 @@
 package de.thm.move.controllers.factorys
 
 import java.net.URI
-import javafx.scene.image.Image
 import javafx.scene.paint.Color
-import javafx.scene.shape.Rectangle
 
 import javafx.scene.image.Image
 
@@ -31,17 +29,17 @@ object ShapeFactory {
 
   def newRectangle(point:Point, width:Double, height:Double): (Color, Color, Int) => ResizableRectangle = {
     val rectangle = new ResizableRectangle(point, width, height)
-    (setDefaultColor(rectangle) _)
+    setDefaultColor(rectangle)
   }
 
   def newLine(start:Point, end:Point, strokeSize:Int): (Color, Color, Int) => ResizableLine = {
     val line = new ResizableLine(start, end, strokeSize)
-    (setDefaultColor(line) _)
+    setDefaultColor(line)
   }
 
   def newCircle(point:Point, width:Double, height:Double): (Color, Color, Int) => ResizableCircle = {
     val circle = new ResizableCircle(point, width, height)
-    (setDefaultColor(circle) _)
+    setDefaultColor(circle)
   }
 
   def newAnchor(point:Point):Anchor = {
@@ -51,17 +49,14 @@ object ShapeFactory {
 
   def newPolygon(points:List[Point]): (Color, Color, Int) => ResizablePolygon = {
     val polygon = ResizablePolygon(points)
-    (setDefaultColor(polygon) _)
+    setDefaultColor(polygon)
   }
 
   def createTemporaryShape(shape:SelectedShape.SelectedShape, p:Point)(stroke:Color): ResizableShape = {
     (shape match {
-      case SelectedShape.Rectangle =>
-        newRectangle(p, 2,2)
-      case SelectedShape.Circle =>
-        newCircle(p,2,2)
-      case SelectedShape.Line =>
-        newLine(p,p, 2)
+      case SelectedShape.Rectangle => newRectangle(p, 2,2)
+      case SelectedShape.Circle => newCircle(p,2,2)
+      case SelectedShape.Line => newLine(p,p, 2)
     })(null, stroke, 2)
   }
 }
