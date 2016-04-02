6ab4ba564d98307b59f475baabff25edf8dbca73
N. Justus <nicola.justus@mni.thm.de>
2016-04-02 15:56:47 +0200

    Fix moving path together with it's anchors
    Because of overriding "def getAnchors" the anchors got always
    newly created and the new anchors got moved, but these anchors never got
    painted onto the drawPanel so you can't see any changes! Using
    "val getAnchors" fixed  the problem so there's only one list of
    anchors!

--- src/main/scala/de/thm/move/views/shapes/ResizablePath.scala
+++ src/main/scala/de/thm/move/views/shapes/ResizablePath.scala
@@ -2,16 +2,19 @@ package de.thm.move.views.shapes
 
 import javafx.scene.paint.Paint
 import javafx.scene.shape.{MoveTo, LineTo, PathElement, Path}
+import de.thm.move.util.BindingUtils
+import javafx.scene.paint.Color
+
 import collection.JavaConversions._
 
 import de.thm.move.views.{MovableAnchor, Anchor}
 
 class ResizablePath(startPoint: MoveTo, elements:List[LineTo]) extends Path(startPoint :: elements) with ResizableShape with ColorizableShape {
 
-  private val observableElements = getElements
+  private val alElements = startPoint :: elements
 
-  override def getAnchors: List[Anchor] =
-    (startPoint :: elements).flatMap {
+  override val getAnchors: List[Anchor] =
+    alElements.flatMap {
       case move:MoveTo =>
         val anchor = new Anchor(move.getX,move.getY) with MovableAnchor
         move.xProperty.bind(anchor.centerXProperty)
@@ -21,8 +24,11 @@ class ResizablePath(startPoint: MoveTo, elements:List[LineTo]) extends Path(star
         val anchor = new Anchor(line.getX,line.getY) with MovableAnchor
         line.xProperty.bind(anchor.centerXProperty)
         line.yProperty.bind(anchor.centerYProperty)
+
         List(anchor)
     }
+
+  BindingUtils.binAnchorsLayoutToNodeLayout(this)(getAnchors:_*)
 
   override def setY(y: Double): Unit = setLayoutY(y)
 
@@ -32,6 +38,6 @@ class ResizablePath(startPoint: MoveTo, elements:List[LineTo]) extends Path(star
 
   override def getX: Double = getLayoutX
 
-  override def getFillColor:Paint = null
-  override def setFillColor(c:Paint):Unit = Unit
+  override def getFillColor:Paint = null /*Path has no fill*/
+  override def setFillColor(c:Paint):Unit = { /*Path has no fill*/ }
 }
