552f1fc1398d84d6b0a8d6cc358349f951456960
N. Justus <nicola.justus@mni.thm.de>
2016-04-04 20:23:30 +0200

    Replace setXX in Anchor by constructor call

--- src/main/scala/de/thm/move/views/Anchor.scala
+++ src/main/scala/de/thm/move/views/Anchor.scala
@@ -10,17 +10,13 @@ import javafx.scene.shape.{Ellipse, Circle}
 import de.thm.move.models.CommonTypes.Point
 import de.thm.move.Global
 
-class Anchor(x:Double, y:Double) extends Ellipse {
+class Anchor(x:Double, y:Double) extends Ellipse(x,y,Anchor.anchorWidth,Anchor.anchorHeight) {
   def this(p:Point) = this(p._1, p._2)
 
   this.getStyleClass.addAll("anchor")
-  this.setCenterX(x)
-  this.setCenterY(y)
-  this.setRadiusX(Anchor.anchorWeight)
-  this.setRadiusY(Anchor.anchorHeight)
 }
 
 object Anchor {
-  val anchorWeight = Global.config.getDouble("anchor.height").getOrElse(2.0)
-  val anchorHeight = Global.config.getDouble("anchor.width").getOrElse(2.0)
+  val anchorWidth = Global.config.getDouble("anchor.width").getOrElse(2.0)
+  val anchorHeight = Global.config.getDouble("anchor.height").getOrElse(2.0)
 }
