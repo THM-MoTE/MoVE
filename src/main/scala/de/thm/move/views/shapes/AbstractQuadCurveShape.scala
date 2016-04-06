b0f63b11a5b92630956970586f976d7d4e65e4c1
N. Justus <nicola.justus@mni.thm.de>
2016-04-06 10:13:45 +0200

    Fix error in start/end-point of smoothed pathes
    Smoothed pathes have straight lines between the original-start-point
    and the start-point of the first bezier curve. This applys also by
    the endpoint of the last bezier curve and the endpoint of the underlying
    path. (see Modelica Spec Page 226 - Sec 18.6.12 Graphical Properties)

--- src/main/scala/de/thm/move/views/shapes/AbstractQuadCurveShape.scala
+++ src/main/scala/de/thm/move/views/shapes/AbstractQuadCurveShape.scala
@@ -8,21 +8,22 @@ import de.thm.move.views.Anchor
 import de.thm.move.views.MovableAnchor
 import de.thm.move.controllers.implicits.FxHandlerImplicits._
 
-abstract class AbstractQuadCurveShape(points:List[Point], closedShape:Boolean) extends Path with ResizableShape with ColorizableShape {
+abstract class AbstractQuadCurveShape(points: List[Point], closedShape: Boolean) extends Path with ResizableShape with ColorizableShape {
 
-  /** Implementation nodes:
-    * The given points represent the normal polygon/path!
-    * - Each point represent a control-point of the becier curve
-    * - the start and end points of the becier curve is the middle point
-    * of 2 given points!
-    *
-    * Example:
-    * You have Points p1,p2,p3
-    * a becier curve between this lines is defined as follows:
-    * startPoint of becier curve = middlePointOf(p1,p2)
-    * endPoint of becier curve = middlePointOf(p2,p3)
-    * controlPoint of becier curve = p2
-    */
+  /**
+   * Implementation nodes:
+   * The given points represent the normal polygon/path!
+   * - Each point represent a control-point of the becier curve
+   * - the start and end points of the becier curve is the middle point
+   * of 2 given points!
+   *
+   * Example:
+   * You have Points p1,p2,p3
+   * a becier curve between this lines is defined as follows:
+   * startPoint of becier curve = middlePointOf(p1,p2)
+   * endPoint of becier curve = middlePointOf(p2,p3)
+   * controlPoint of becier curve = p2
+   */
 
   val reversedP = points.reverse
   /*the path behind this element; the points gets adjusted whenever someone
@@ -31,34 +32,45 @@ abstract class AbstractQuadCurveShape(points:List[Point], closedShape:Boolean) e
   private val underlyingPolygonPoints = reversedP.toArray
 
   val curves = adjustPath(reversedP.toArray)
-  this.getElements.addAll(curves:_*)
+  this.getElements.addAll(curves: _*)
 
-  private def adjustPath(points:Array[Point]): List[PathElement] = {
-    val (stX,stY) = points.head
-    val (tmpX,tmpY) = GeometryUtils.middleOfLine(points.head, points(1)) //point between head & points(1)
-    val start = new MoveTo(tmpX,tmpY) //shfit path to starting-point
-    val end = new QuadCurveTo(stX,stY, tmpX,tmpY)
+  private def adjustPath(points: Array[Point]): List[PathElement] = {
+    val (stX, stY) = points.head
+    val (tmpX, tmpY) = GeometryUtils.middleOfLine(points.head, points(1)) //point between head & points(1)
+    /**
+     * shfit path to starting-point if this is a closedShape (e.g. polygon)
+     * if not draw a line between original startpoint and starting-point
+     * for first bezier curve
+     */
+    val start =
+      if (closedShape) List(new MoveTo(tmpX, tmpY))
+      else List(new MoveTo(stX, stY), new LineTo(tmpX, tmpY))
+    val end = new QuadCurveTo(stX, stY, tmpX, tmpY)
 
-    val xs = start::(for(idx <- 1 until points.size) yield {
+    val xs = start ::: (for (idx <- 1 until points.size) yield {
       //(startPoint for this becier curve = last added point of the path)
       val (ctrlX, ctrlY) = points(idx) //ctrlPoint := this point
       //endPoint := middleOf(thisPoint, nextPoint)
       val (endX, endY) =
-        if(idx+1 < points.size) GeometryUtils.middleOfLine(points(idx), points(idx + 1))
+        if (idx + 1 < points.size) GeometryUtils.middleOfLine(points(idx), points(idx + 1))
         else GeometryUtils.middleOfLine(points(idx), points.head)
 
-      val curve = new QuadCurveTo(ctrlX,ctrlY, endX,endY)
-      curve
+      new QuadCurveTo(ctrlX, ctrlY, endX, endY)
     }).toList
 
-    if(closedShape) xs ::: List(end)
-    else xs
+    /**
+     * close the path if it's a closedShape (e.g. polygon)
+     * if not draw a line between last beziercurve end-point & endpoint of
+     * original path
+     */
+    if (closedShape) xs ::: List(end)
+    else xs.init ::: List(new LineTo(points.last.x, points.last.y))
   }
 
-  def getUnderlyingPolygonPoints:List[Point] = underlyingPolygonPoints.toList
+  def getUnderlyingPolygonPoints: List[Point] = underlyingPolygonPoints.toList
 
   override val getAnchors: List[Anchor] =
-    (for(
+    (for (
       idx <- 0 until underlyingPolygonPoints.size;
       ctrlP = underlyingPolygonPoints(idx)
     ) yield {
@@ -66,22 +78,22 @@ abstract class AbstractQuadCurveShape(points:List[Point], closedShape:Boolean) e
       /* Everytime this shape get's resized the path get's replaced
        * by a new path
        */
-      anchor.centerXProperty.addListener { (_:Number, newV:Number) =>
-        underlyingPolygonPoints(idx) = (newV.doubleValue,underlyingPolygonPoints(idx).y)
+      anchor.centerXProperty.addListener { (_: Number, newV: Number) =>
+        underlyingPolygonPoints(idx) = (newV.doubleValue, underlyingPolygonPoints(idx).y)
         this.getElements.clear()
-        this.getElements.addAll(adjustPath(underlyingPolygonPoints):_*)
+        this.getElements.addAll(adjustPath(underlyingPolygonPoints): _*)
         ()
       }
-      anchor.centerYProperty.addListener{ (_:Number, newV:Number) =>
+      anchor.centerYProperty.addListener { (_: Number, newV: Number) =>
         underlyingPolygonPoints(idx) = (underlyingPolygonPoints(idx).x, newV.doubleValue)
         this.getElements.clear()
-        this.getElements.addAll(adjustPath(underlyingPolygonPoints):_*)
+        this.getElements.addAll(adjustPath(underlyingPolygonPoints): _*)
         ()
       }
       anchor
     }).toList
 
-  BindingUtils.binAnchorsLayoutToNodeLayout(this)(getAnchors:_*)
+  BindingUtils.binAnchorsLayoutToNodeLayout(this)(getAnchors: _*)
   override def setY(y: Double): Unit = setLayoutY(y)
   override def getY: Double = getLayoutY
   override def setX(x: Double): Unit = setLayoutX(x)
