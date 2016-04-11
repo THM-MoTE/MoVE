8e3b9e69e1e1cc4397536a099caf2c8c1169b1f4
N. Justus <nicola.justus@mni.thm.de>
2016-04-11 11:09:56 +0200

    Add test with real-world example image

---
+++ src/test/scala/de/thm/move/loader/parser/FullParserTest.scala
@@ -0,0 +1,90 @@
+package de.thm.move.loader.parser
+
+
+import org.junit.Assert._
+import org.junit.Test
+
+import de.thm.move.loader.parser.PropertyParser._
+import de.thm.move.loader.parser.ast._
+
+class FullParserTest {
+  @Test
+  def fullTest:Unit = {
+    val formatted =
+    """
+    model test3
+      annotation(
+      Icon (
+        coordinateSystem(
+          extent = {{0,0},{856,504}}
+        ),
+        graphics = {
+          Bitmap(
+            extent = {{466,493}, {817,205}},
+            fileName = "modelica://test3/quokka.png"
+          ),
+          Line(
+            points = {{566,80},{511,97},{596,135},{542,186},{431,175},{414,109},{422,70}},
+            color = {230,77,77},
+            pattern = LinePattern.DashDotDot,
+            thickness = 4.0,
+            smooth = Smooth.Bezier
+          ),
+          Ellipse(
+            lineColor = {102,77,179},
+            fillColor = {204,204,51},
+            lineThickness = 4.0,
+            pattern = LinePattern.DashDot,
+            fillPattern = FillPattern.Solid,
+            extent = {{134,477},{326,384}},
+            endAngle = 360
+          ),
+          Rectangle(
+            lineColor = {0,0,0},
+            fillColor = {204,204,51},
+            lineThickness = 4.0,
+            pattern = LinePattern.Solid,
+            fillPattern = FillPattern.Solid,
+            extent = {{205,179}, {348,36}}
+          ),
+          Polygon(
+            points = {{660,393},{547,335},{637,298},{578,242},{654,237}},
+                     lineColor = {0,0,0},
+            fillColor = {255,0,0},
+            lineThickness = 4.0,
+            pattern = LinePattern.Solid,
+            fillPattern = FillPattern.Solid,
+            smooth = Smooth.Bezier
+          ),
+          Polygon(
+            points = {{434,235},{358,240},{417,296},{327,333},{440,391}},
+                     lineColor = {0,0,0},
+            fillColor = {255,0,0},
+            lineThickness = 4.0,
+            pattern = LinePattern.Solid,
+            fillPattern = FillPattern.Solid,
+            smooth = Smooth.Bezier
+          ),
+          Line(
+            points = {{73,469},{143,215}},
+            color = {0,0,0},
+            pattern = LinePattern.Dash,
+            thickness = 4.0
+          )
+        })
+      );
+    end test3;
+    """
+
+    withParseSuccess(formatted)
+
+    val oneLine =
+    """
+    model test4
+annotation( Icon (  coordinateSystem(  extent = {{0,0},{856,504}}  ), graphics = { Bitmap(  extent = {{466,493}, {817,205}},  fileName = "modelica://test4/quokka.png"  ), Line(  points = {{566,80},{511,97},{596,135},{542,186},{431,175},{414,109},{422,70}},  color = {230,77,77},  pattern = LinePattern.DashDotDot,  thickness = 4.0,  smooth = Smooth.Bezier  ), Ellipse(  lineColor = {102,77,179},  fillColor = {204,204,51},  lineThickness = 4.0,  pattern = LinePattern.DashDot,  fillPattern = FillPattern.Solid,  extent = {{134,477},{326,384}},  endAngle = 360  ), Rectangle(  lineColor = {0,0,0},  fillColor = {204,204,51},  lineThickness = 4.0,  pattern = LinePattern.Solid,  fillPattern = FillPattern.Solid,  extent = {{205,179}, {348,36}}  ), Polygon(  points = {{660,393},{547,335},{637,298},{578,242},{654,237}},           lineColor = {0,0,0},  fillColor = {255,0,0},  lineThickness = 4.0,  pattern = LinePattern.Solid,  fillPattern = FillPattern.Solid,  smooth = Smooth.Bezier  ), Polygon(  points = {{434,235},{358,240},{417,296},{327,333},{440,391}},           lineColor = {0,0,0},  fillColor = {255,0,0},  lineThickness = 4.0,  pattern = LinePattern.Solid,  fillPattern = FillPattern.Solid,  smooth = Smooth.Bezier  ), Line(  points = {{73,469},{143,215}},  color = {0,0,0},  pattern = LinePattern.Dash,  thickness = 4.0  )}) );
+end test4;
+    """
+
+    withParseSuccess(oneLine)
+  }
+}
