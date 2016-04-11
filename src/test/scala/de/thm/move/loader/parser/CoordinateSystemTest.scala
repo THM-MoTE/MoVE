73e2640ae55a8837c4d0df809d756fadc49bb5fd
N. Justus <nicola.justus@mni.thm.de>
2016-04-11 10:37:01 +0200

    Add test for coordination-system

---
+++ src/test/scala/de/thm/move/loader/parser/CoordinateSystemTest.scala
@@ -0,0 +1,38 @@
+package de.thm.move.loader.parser
+
+import org.junit.Assert._
+import org.junit.Test
+
+import de.thm.move.loader.parser.PropertyParser._
+import de.thm.move.loader.parser.ast._
+
+class CoordinateSystemTest {
+
+  @Test
+  def minimalSystem: Unit = {
+    val str =
+      """
+        |model SimpleModel
+        | annotation(
+        |   Icon (
+        |      coordinateSystem (
+        |      extent = {{0,0},{756,504}}
+        |      ),
+        |     graphics = {}
+        |   )
+        | );
+        |end SimpleModel;
+      """.stripMargin
+
+      val exp =
+        Model("SimpleModel",
+          List(
+              Icon(
+                Some(CoordinateSystem( ((0,0),(756,504)) )),
+                List()
+                )
+            )
+        )
+    assertEquals(exp, withParseSuccess(str))
+  }
+}
