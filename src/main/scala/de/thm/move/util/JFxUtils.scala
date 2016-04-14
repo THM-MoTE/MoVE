566264015be9daef23058343ff9620514508eb0d
N. Justus <nicola.justus@mni.thm.de>
2016-04-14 11:49:47 +0200

    Create withMovable,withResizable function

--- src/main/scala/de/thm/move/util/JFxUtils.scala
+++ src/main/scala/de/thm/move/util/JFxUtils.scala
@@ -9,6 +9,7 @@ import de.thm.move.controllers.implicits.FxHandlerImplicits._
 import javafx.scene.Node
 import javafx.scene.control.ChoiceBox
 import de.thm.move.views.Anchor
+import de.thm.move.views.shapes.{ResizableShape, MovableShape}
 
 /** General utils for working with JavaFx. */
 object JFxUtils {
@@ -27,5 +28,17 @@ object JFxUtils {
       anchor.layoutXProperty().bind(node.layoutXProperty())
       anchor.layoutYProperty().bind(node.layoutYProperty())
     }
+  }
+
+  def withMovableElement[A](n:Node)(fn: MovableShape => A):A = (n, n.getParent) match {
+    case (_,ms:MovableShape) => fn(ms)
+    case (ms:MovableShape,_) => fn(ms)
+    case _ => throw new IllegalArgumentException(s"that's not a movableShape: $n")
+  }
+
+  def withResizableElement[A](n:Node)(fn: ResizableShape => A):A = (n, n.getParent) match {
+    case (_,ms:ResizableShape) => fn(ms)
+    case (ms:ResizableShape,_) => fn(ms)
+    case _ => throw new IllegalArgumentException(s"that's not a resizableShape: $n")
   }
 }
