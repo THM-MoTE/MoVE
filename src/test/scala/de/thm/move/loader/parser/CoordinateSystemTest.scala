d455ba5378b1b21a6bf427355d639eaa7490abdf
N. Justus <nicola.justus@mni.thm.de>
2016-04-14 15:23:35 +0200

    Add test for fullCoordinateSystem

--- src/test/scala/de/thm/move/loader/parser/CoordinateSystemTest.scala
+++ src/test/scala/de/thm/move/loader/parser/CoordinateSystemTest.scala
@@ -35,4 +35,37 @@ class CoordinateSystemTest {
         )
     assertEquals(exp, withParseSuccess(str))
   }
+
+  @Test
+  def fullSystem: Unit = {
+    val str =
+      """
+        |model SimpleModel
+        | annotation(
+        |   Icon (
+        |      coordinateSystem (
+        |      extent = {{0,0},{756,504}},
+        |      initialScale = 0.5,
+        |      preserveAspectRatio = false
+        |      ),
+        |     graphics = {}
+        |   )
+        | );
+        |end SimpleModel;
+      """.stripMargin
+
+      val exp =
+        Model("SimpleModel",
+          List(
+              Icon(
+                Some(CoordinateSystem(
+                  ((0,0),(756,504)),
+                  false, 0.5
+                  )),
+                List()
+                )
+            )
+        )
+    assertEquals(exp, withParseSuccess(str))
+  }
 }
