ddd93c7eb1457fc980e18e7e16ee39d4ded39331
N. Justus <nicola.justus@mni.thm.de>
2016-04-26 11:30:58 +0200

    Set values for FontFamilyChooser,FontSizeChooser

--- src/main/scala/de/thm/move/controllers/TextToolbarCtrl.scala
+++ src/main/scala/de/thm/move/controllers/TextToolbarCtrl.scala
@@ -8,10 +8,17 @@ import java.util.ResourceBundle
 import java.net.URL
 import javafx.event.ActionEvent
 import javafx.scene.control._
+import javafx.scene.text.Font
+import javafx.collections.FXCollections
 import javafx.fxml.{FXML, FXMLLoader, Initializable}
 import implicits.FxHandlerImplicits._
 
+import scala.collection.JavaConverters._
+
 class TextToolbarCtrl extends Initializable {
+
+  @FXML var fontFamilyChooser:ChoiceBox[String] = _
+  @FXML var fontSizeChooser:ChoiceBox[Int] = _
 
   @FXML var fontColorLbl:Label = _
   @FXML var fontColorChooser:ColorPicker = _
@@ -20,5 +27,13 @@ class TextToolbarCtrl extends Initializable {
     fontColorChooser.setOnAction { _:ActionEvent =>
       fontColorLbl.setTextFill(fontColorChooser.getValue)
     }
+
+    val fontNames = Font.getFamilies().asScala.distinct.asJava
+    fontFamilyChooser.setItems(FXCollections.observableArrayList(fontNames))
+    fontFamilyChooser.setValue(Font.font("Arial").getFamily)
+
+    val sizesList = (8 until 72 by 2).asJava
+    fontSizeChooser.setItems(FXCollections.observableArrayList(sizesList))
+    fontSizeChooser.setValue(12)
   }
 }
