93141a7eee557d44d0f9167489d3555056e92c4e
N. Justus <nicola.justus@mni.thm.de>
2016-05-02 20:39:16 +0200

    Refactor AbstractQuadCurve using PathLike

--- src/main/scala/de/thm/move/views/shapes/QuadCurvePath.scala
+++ src/main/scala/de/thm/move/views/shapes/QuadCurvePath.scala
@@ -7,8 +7,11 @@ package de.thm.move.views.shapes
 import de.thm.move.models.CommonTypes.Point
 import javafx.scene.paint.Paint
 
+import de.thm.move.views.Anchor
+
 class QuadCurvePath(points:List[Point])
   extends AbstractQuadCurveShape(points, false) {
+  override val getAnchors:List[Anchor] = genAnchors
   override def getFillColor:Paint = null /*Path has no fill*/
   override def setFillColor(c:Paint):Unit = { /*Path has no fill*/ }
 
