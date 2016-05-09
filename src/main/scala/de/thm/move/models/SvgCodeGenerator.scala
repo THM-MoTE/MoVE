e3d0c21deb5246a92efff5f3313daa83fee15621
N. Justus <nicola.justus@mni.thm.de>
2016-05-09 11:42:25 +0200

    Start implementing rectangle svg-codegen

--- src/main/scala/de/thm/move/models/SvgCodeGenerator.scala
+++ src/main/scala/de/thm/move/models/SvgCodeGenerator.scala
@@ -8,22 +8,38 @@ import java.nio.file.{Files, Path}
 import javafx.scene.Node
 
 import de.thm.move.Global._
-import de.thm.move.views.shapes.ResizableShape
+import de.thm.move.views.shapes._
 
 import scala.xml.{Elem, NodeSeq}
 
 class SvgCodeGenerator {
 
   def generateShapes(shapes:List[Node], width:Double, height:Double): Elem = {
-    <svg width="@width" height="@height">
-      shapes.map(generateShape)
+    <svg width={width.toString} height={height.toString}>
+      {shapes.map(generateShape)}
     </svg>
   }
   def generateShape(shape:Node): Elem = shape match {
-    case _ => <node></node>
+    case rect:ResizableRectangle => genRectangle(rect)
+    case _ => throw new IllegalArgumentException(s"Can't generate svg code for: $shape")
+  }
+
+  private def genRectangle(rectangle:ResizableRectangle): Elem = {
+    <rect
+      x={rectangle.getX.toString}
+      y={rectangle.getY.toString}
+      width={rectangle.getWidth.toString}
+      height={rectangle.getHeight.toString}
+      />
   }
 
   def writeToFile(str:String)(target:Path): Unit = {
     val writer = Files.newBufferedWriter(target, encoding)
+
+    try {
+      writer.write(str)
+    } finally {
+      writer.close()
+    }
   }
 }
