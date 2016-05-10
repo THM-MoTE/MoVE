a58dac6d5e41ea3ecc5e45cc27aaa1deabda085a
N. Justus <nicola.justus@mni.thm.de>
2016-05-10 09:56:03 +0200

    Create encodeBase64(),encodeBase64String(),copy()

--- src/main/scala/de/thm/move/util/ResourceUtils.scala
+++ src/main/scala/de/thm/move/util/ResourceUtils.scala
@@ -4,8 +4,11 @@
 
 package de.thm.move.util
 
+import java.util.Base64
 import java.net.URI
 import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.Files
 import javafx.scene.paint.Color
 import de.thm.move.Global._
 
@@ -22,4 +25,24 @@ object ResourceUtils {
 
   def asColor(key:String): Option[Color] =
     config.getString(key).map(Color.web)
+
+  /** Encodes the given bytes as base64 */
+  def encodeBase64(bytes:Array[Byte]): Array[Byte] = {
+    val encoder = Base64.getEncoder
+    encoder.encode(bytes)
+  }
+
+  /** Encodes the given bytes as base64 and returns it as a string.
+    * @see [ResourceUtils#encodeBase64]
+    */
+  def encodeBase64String(bytes:Array[Byte]): String =
+    new String(encodeBase64(bytes), encoding)
+
+  /** Copies src into the parent-directory of target */
+  def copy(src:URI, target:URI): Unit = {
+    val targetPath = Paths.get(target).getParent
+    val srcPath = Paths.get(src)
+    val filename = srcPath.getFileName
+    Files.copy(srcPath, targetPath.resolve(filename))
+  }
 }
