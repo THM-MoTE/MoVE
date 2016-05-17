8c5aa8306f16a9e4164e61e42db819ef2b33b3b4
N. Justus <nicola.justus@mni.thm.de>
2016-05-17 13:34:09 +0200

    Add doc for Validation.

--- src/main/scala/de/thm/move/util/Validation.scala
+++ src/main/scala/de/thm/move/util/Validation.scala
@@ -4,19 +4,35 @@
 
 package de.thm.move.util
 
+/** A monad for calculating values which could return a value with warnings.
+  * @tparam A the type of the value
+  * @tparam E the type of the warning
+  */
 sealed trait Validation[+A, +E] {
+  /** Maps the value with the given function fn, copying the warnings from this Validation to the
+    * returning Validation.
+    */
   def map[B](fn: A => B): Validation[B, E]
+  /** Combines this Validation with the Validation returned from calling fn.
+    *
+    * fn is called with the value from this Validation. If this Validation and the returning Validation
+    * from fn contains warnings they will be joined.
+    */
   def flatMap[B, EE >: E](fn: A => Validation[B,EE]): Validation[B, EE]
+  /** Returns the value of this Validation */
   def getValue:A
+  /** Returns the warnings of this Validation */
   def getWarnings:List[E]
 }
 
+/** A validation without a warning. */
 case class ValidationSuccess[A](value:A) extends Validation[A, Nothing] {
   def map[B](fn: A => B): Validation[B, Nothing] = ValidationSuccess(fn(value))
   def flatMap[B, EE >: Nothing](fn: A => Validation[B,EE]): Validation[B, EE] = fn(value)
   def getValue:A = value
   def getWarnings:List[Nothing] = Nil
 }
+/** A validation with warnings. */
 case class ValidationWarning[A, E](value:A, warnings:List[E]) extends Validation[A, E] {
   def map[B](fn: A => B): Validation[B, E] = ValidationWarning(fn(value), warnings)
   def flatMap[B, EE >: E](fn: A => Validation[B,EE]): Validation[B, EE] = fn(value) match {
@@ -28,8 +44,10 @@ case class ValidationWarning[A, E](value:A, warnings:List[E]) extends Validation
 }
 
 object Validation {
+  /** Returns a ValidationSuccess with the given value a */
   def apply[A, E](a:A):Validation[A,E] = ValidationSuccess(a)
 }
 object ValidationWarning {
+  /** Returns a ValidationWarning with the given value a and the given warning w wrapped inside a list. */
   def apply[A, E](a:A, w:E):ValidationWarning[A,E] = ValidationWarning(a, List(w))
 }
