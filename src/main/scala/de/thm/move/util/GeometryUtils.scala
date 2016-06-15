bdddd41503d30b3bb7b24b9562afd292a4311816
N. Justus <nicola.justus@mni.thm.de>
2016-06-15 11:11:48 +0200

    Create calculateRotationOffset()

--- src/main/scala/de/thm/move/util/GeometryUtils.scala
+++ src/main/scala/de/thm/move/util/GeometryUtils.scala
@@ -5,6 +5,8 @@
 package de.thm.move.util
 
 import de.thm.move.models.CommonTypes.Point
+import javafx.geometry.Point2D
+import javafx.scene.transform.{Affine, Rotate, Transform}
 
 import scala.math.BigDecimal
 
@@ -31,5 +33,21 @@ object GeometryUtils {
     if(v % x == 0) Some(v) //v = multiple of x
     else if(rem % 0.5 == 0) None //remainder is a.5 => no nearest value
     else Some(multiple*x)
+  }
+
+  def calculateRotationOffset(cOld: Point2D, cNew: Point2D, deg: Double, isoCorner: Point2D): Point2D =  {
+    val r = new Rotate(deg, cOld.getX(), cOld.getY());
+    val rotCorner = r.transform(isoCorner);
+    val relCorner = isoCorner.subtract(cNew);
+    // find new center for new shape
+    val c = Math.cos(Math.toRadians(deg));
+    val s = Math.sin(Math.toRadians(deg));
+    val cNewTrans = new Point2D(
+            rotCorner.getX() - c * relCorner.getX() + s * relCorner.getY(),
+            rotCorner.getY() - c * relCorner.getY() - s * relCorner.getX()
+    );
+    // find delta vector between old and new center
+    val delta = cNewTrans.subtract(cNew);
+    delta;
   }
 }
