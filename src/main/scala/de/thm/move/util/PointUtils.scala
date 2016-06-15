fe08309a39d070100a192f84c94aecc3b8d85578
N. Justus <nicola.justus@mni.thm.de>
2016-06-15 11:11:19 +0200

    Create / -function on Points

--- src/main/scala/de/thm/move/util/PointUtils.scala
+++ src/main/scala/de/thm/move/util/PointUtils.scala
@@ -24,6 +24,8 @@ object PointUtils {
     @inline
     final def -(that:Point):Point = (p._1-that._1, p._2-that._2)
     @inline
+    final def /(that:Point):Point = (p._1/that._1, p._2/that._2)
+    @inline
     final def +(that:Point):Point = (p._1+that._1, p._2+that._2)
     @inline
     final def map[A](fn: Double => A):(A,A) = (fn(p._1), fn(p._2))
