5b1042dd4afd27bfda1e3ccc2c35a7ccd76fcdee
N. Justus <nicola.justus@mni.thm.de>
2016-11-07 17:10:29 +0100

    Add -version parameter #98

--- src/main/scala/de/thm/move/MoveApp.scala
+++ src/main/scala/de/thm/move/MoveApp.scala
@@ -71,22 +71,32 @@ class MoveApp extends Application {
 }
 
 object MoveApp {
+  val name = build.ProjectInfo.name
+  val copyright = build.ProjectInfo.copyright
+  val version = build.ProjectInfo.version
+  val licenseName = build.ProjectInfo.licenseName
+
+  def printVersion(): Unit = {
+    val versionInfo = s"""$name - V$version - $copyright - $licenseName"""
+    println(versionInfo)
+  }
+
   def help(): Unit = {
-    val name = build.ProjectInfo.name
-    val version = build.ProjectInfo.version
     val helpMsg =
       s"""Usage:
       |\tjava -jar $name-$version.jar
       |\tjava -jar $name-$version.jar [filename]
       |
       |Options:
-      |\t-help\t\tDisplay this help message""".stripMargin
+      |\t-help\t\tDisplay this help message
+      |\t-version\tPrint version & exit""".stripMargin
     println(helpMsg)
   }
 
   def main(args: Array[String]): Unit = {
     args.toList match {
       case "-help" :: _ => help()
+      case "-version" :: _ => printVersion()
       case path :: _ if !Files.exists(Paths.get(path)) || !Files.isRegularFile(Paths.get(path)) =>
         println(s"WARNING: The given file [$path] wasn't a file or doesn't exist!")
         Application.launch(classOf[MoveApp])
