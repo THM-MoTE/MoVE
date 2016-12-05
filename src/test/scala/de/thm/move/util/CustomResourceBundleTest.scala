3bb57eb3418c0f74a745caaa09c4e6f5e3b15067
N. Justus <nicola.justus@mni.thm.de>
2016-12-05 14:10:42 +0100

    Fix tests for ResourceBundle

--- src/test/scala/de/thm/move/util/CustomResourceBundleTest.scala
+++ src/test/scala/de/thm/move/util/CustomResourceBundleTest.scala
@@ -3,13 +3,18 @@ package de.thm.move.util
 import java.util.{MissingResourceException, ResourceBundle}
 
 import de.thm.move.MoveSpec
+import org.scalatest.BeforeAndAfterAll
 
-class CustomResourceBundleTest extends MoveSpec {
+class CustomResourceBundleTest extends MoveSpec with BeforeAndAfterAll {
+  override def beforeAll() = {
+    java.util.Locale.setDefault(java.util.Locale.ENGLISH)
+  }
+
   val locale = java.util.Locale.ENGLISH
   val bundle = new CustomResourceBundle(List("i18n/messages"), locale)
 
   "A CustomResourceBundle" should "retrieve keys based on the locale" in {
-    bundle.getString("file.new") shouldBe "New"
+    bundle.getString("file.new") shouldBe "New file"
     bundle.getString("edit.undo") shouldBe "Undo"
   }
   it should "not retrieve unknown keys" in {
@@ -18,14 +23,12 @@ class CustomResourceBundleTest extends MoveSpec {
     an [MissingResourceException] should be thrownBy bundle.getString("tiger")
   }
   it should "retrieve resources from multiple bundles" in {
-    val locale = java.util.Locale.ENGLISH
     val bundle = new CustomResourceBundle(List("i18n/messages", "fonts/fontawesome"), locale)
     bundle.getString("fa.square") shouldBe "\uf0c8"
     bundle.getString("fa.file.exit") shouldBe "\uf00d"
   }
 
   it should "not retrieve unknown keys from resources with multiple bundles" in {
-    val locale = java.util.Locale.ENGLISH
     val bundle = new CustomResourceBundle(List("i18n/messages", "fonts/fontawesome"), locale)
     an [MissingResourceException] should be thrownBy bundle.getString("fa.test")
   }
